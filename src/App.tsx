6c4bc2674c03bb7edd48cb8fabc63c3e6ffd6609
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-12-06 19:23:30 +0100

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import GlobalStyles from './styles/GlobalStyles';
 /* import Lesson07 from './lessons/lesson07/Lesson07'; */
 /* import Lesson08 from './lessons/Lesson08/Lesson08'; */
 
+//homeworks
+/* import Homework07 from 'homeworks/Homework07/Homework07'; */
+/* import Homework08 from 'homeworks/Homework08/Homework08'; */
+/* import Homework09 from 'homeworks/Homework09/Homework09'; */
 
 //consultations
 /* import Consultation04 from './consultations/Consultation04/Consultation04'; */
@@ -22,10 +26,11 @@ function App() {
       {/* <Consultation04/> */}
       {/* <Homework07/> */}
       {/* <Lesson08/> */}
-      <Homework08/>
+      {/* <Homework08/> */}
       {/* <Lesson09/> */}
+      {/* <Homework09/> */}
       {/* <Consultation05/> */}
-      <HW09Lesson/>
+      {/* <HW09Lesson/> */}
 
     </>
   );
